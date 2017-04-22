b2d7f4867be8d114b3a8910d413966166637783f
samidarko <samidarko@gmail.com>
2017-04-22 17:08:07 +0800

    work but no chunks

--- src/main/scala/algorithms/Zip.scala
+++ src/main/scala/algorithms/Zip.scala
@@ -7,78 +7,37 @@ import java.util.zip.{ZipEntry, ZipInputStream, ZipOutputStream}
   */
 class Zip extends Compression{
 
-  private class ChunkedZippedOutputStream(out: File, chunkSize : Int) {
+//  private var currentChunkIndex : Int = 0
+//
+//  private def getChunkName : String = {
+//    val chunkName = s"archive.part.$currentChunkIndex.zip"
+//    currentChunkIndex += 1
+//    chunkName
+//  }
 
-    // TODO test chunkSize > 0
+  def compress(in: File, out: File, size: Int): Unit = {
 
-    private var zipOutputStream : ZipOutputStream = _
-    private var currentSize : Long = 0
-    private var currentChunkIndex : Int = 0
 
-    @throws(classOf[FileNotFoundException])
-    private def constructNewStream() : Unit = {
-      zipOutputStream = new ZipOutputStream(new FileOutputStream(new File(out, constructCurrentPartName)))
-      currentChunkIndex += 1
-      currentSize = 0
-    }
-
-    @throws(classOf[IOException])
-    private def closeStream() : Unit = {
-      zipOutputStream.close()
-    }
-
-    private def constructCurrentPartName : String = {
-      s"archive.part.$currentChunkIndex.zip"
-    }
-
-    @throws(classOf[IOException])
-    def addEntry(zipEntry: ZipEntry) : Unit = {
-      val entrySize = zipEntry.getCompressedSize
-      if ((currentSize + entrySize) > chunkSize) {
-        closeStream()
-        constructNewStream()
-      } else {
-        currentSize += entrySize
-        zipOutputStream.putNextEntry(zipEntry)
-      }
-
-    }
-
-    constructNewStream()
-
-  }
-
-  override def compress(in: File, out: File, size: Int): Unit = {
-
+//    val bufferSize = 1024 * 1024 * size
+//    val buffer = new Array[Byte](bufferSize)
+//
     val files = FileSystem.getListOfFiles(in)
 
-    val czos = new ChunkedZippedOutputStream(out, 16)
-
-    files.foreach {
-      file => czos.addEntry(new ZipEntry(file.toString))
-    }
-
-
-  }
-
-  def compressOld(in: File, out: File, size: Int): Unit = {
-    val files = FileSystem.getListOfFiles(in)
-    // TODO test if `out` is directory
-    val zip = new ZipOutputStream(new FileOutputStream(out))
+//    var zos = new ZipOutputStream(new FileOutputStream(new File(out, getChunkName)))
+    val zos = new ZipOutputStream(new FileOutputStream(new File(out, "archive.zip")))
 
     files.foreach { name =>
-      zip.putNextEntry(new ZipEntry(name.toString))
-      val in = new BufferedInputStream(new FileInputStream(name))
-      var b = in.read()
-      while (b > -1) {
-        zip.write(b)
-        b = in.read()
-      }
-      in.close()
-      zip.closeEntry()
-    }
-    zip.close()
 
+      zos.putNextEntry(new ZipEntry(name.toString))
+
+      val bis = new BufferedInputStream(new FileInputStream(name))
+
+      Stream.continually(bis.read).takeWhile(_ > -1).foreach(zos.write)
+
+      bis.close()
+      zos.closeEntry()
+    }
+    zos.close()
   }
 
   override def extract(in: File, out: File): Unit = {
@@ -90,22 +49,14 @@ class Zip extends Compression{
       .continually(zis.getNextEntry)
       .takeWhile(_ != null)
       .foreach { entry =>
-        val fileName = entry.getName
-        val newFile = new File(out, fileName)
+        val newFile = new File(out, entry.getName)
         new File(newFile.getParent).mkdirs()
-        if (! fileName.endsWith("/")) {
-          val fos = new FileOutputStream(newFile)
+        val fos = new FileOutputStream(newFile)
 
-          var len: Int = zis.read(buffer)
+        Stream.continually(zis.read(buffer))
+          .takeWhile(_ > 0).foreach(len => fos.write(buffer, 0, len))
 
-          while (len > 0) {
-
-            fos.write(buffer, 0, len)
-            len = zis.read(buffer)
-          }
-
-          fos.close()
-        }
+        fos.close()
       }
     zis.closeEntry()
     zis.close()
